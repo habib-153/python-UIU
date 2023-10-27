627274ce176a845567e5dbbb4523ecc726e189c1
habib-153 <h.r.sihab155@gmail.com>
2023-10-28 02:10:15 +0600

    6 modified

--- ProblemSheet_2/p6.py
+++ ProblemSheet_2/p6.py
@@ -1,10 +1,10 @@
-n = int(input("Enter a number: "))
+n = int(input())
 
 reverse = ""
 remainder = 0
-# zero hoa porjonto loop chalabo e jonno while loop
-while n > 0:
-  remainder = n % 10
-  reverse += str(remainder) + ""
-  n = n // 10
+# zero na hole loop chalabo e jonno while loop
+while(n > 0):
+    remainder = n % 10
+    reverse += str(remainder) + ""
+    n = n // 10
 print(reverse)
