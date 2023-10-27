49c8dedd0e66a45c21051cbcfde1f02b87fa7a0e
habib-153 <h.r.sihab155@gmail.com>
2023-10-28 00:25:19 +0600

    problemset 1 problems reaarageed

--- Extra/test.py
+++ Extra/test.py
@@ -17,7 +17,28 @@
 # motor.sort (key=lambda x: x [0] if isinstance (x, list) else x)
 # print (motor)
 
-motor = ["honda", "yamaha", ["kawasaki", "bajaj"], "suzuki", "hero"]
-sorted_motor = [sorted (x, key=str) if isinstance (x, list) else x for x in motor]
-sorted_motor = sorted (sorted_motor, key=str)
-print(sorted_motor)
+# motor = ["honda", "yamaha", ["kawasaki", "bajaj"], "suzuki", "hero"]
+# sorted_motor = [sorted (x, key=str) if isinstance (x, list) else x for x in motor]
+# sorted_motor = sorted (sorted_motor, key=str)
+# print(sorted_motor)
+
+
+    # N = int(input())
+    # # Initialize a variable to store the sum
+    # sum = 0
+    # # Loop from 1 to N
+    # for i in range(1, N + 1):
+    #     # Calculate the term at each index
+    #     term = i * (-1) ** i
+    #     # Add the term to the sum
+    #     sum += term
+    # # Print the sum
+    # print(sum)
+
+
+# num = int(input())
+# if(num+1)%2 == 1:
+#     index_value = num+1
+# else:
+#     index_value = -(num+1)
+# print(index_value)
--- ProblemSheet_2/p4.py
+++ ProblemSheet_2/p4.py
@@ -11,32 +11,10 @@
 
 # print("Average: {:.4f}".format(average))
 
-# n = int(input())
-# number = input().split()[:n]
-# sum = 0
-# for i in number:
-#     sum = sum + float(i)
-# avg = sum / n
-# print(avg)
-
-
-N = int(input("Enter the number of inputs: "))
-
-# Get a list of strings from the input by splitting on spaces
-numbers = input("Enter the numbers in one line: ").split()
-
-# Convert the strings to floats using map
-numbers = list(map(float, numbers))
-
-# Check if the number of inputs matches N
-if len(numbers) != N:
-    print("Invalid input")
-else:
-    # Calculate the sum of the numbers in the list
-    total = sum(numbers)
-
-    # Calculate the average by dividing the sum by N
-    average = total / N
-
-    # Print the average with four decimal places
-    print("Average: {:.4f}".format(average))
+n = int(input())
+number = input().split()[:n]
+sum = 0
+for i in number:
+    sum = sum + float(i)
+avg = sum / n
+print("Average: {:.4f}".format(avg))
