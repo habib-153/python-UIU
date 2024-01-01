7fea13c6e609f8adda580ecd378919ee685677d4
habib-153 <h.r.sihab155@gmail.com>
2024-01-01 11:07:08 +0600

    Some change in p18

--- Problem_Sets/Function Related Problems_Sakkhar Sir/p18.py
+++ Problem_Sets/Function Related Problems_Sakkhar Sir/p18.py
@@ -13,19 +13,17 @@ def ScalarMultiply(mat, n):
             k = j*n
             print(k, end="\t")
         print()
+        
 def InputMatrix():
-    """Take inupt for the matrix from the user"""
     for i in range(3):
         user_input = input().split()
         lst = [int(x) for x in user_input][:5]
         matrix.append(lst)
+
 if __name__=="__main__":
 
     matrix = []
-
     first_call = InputMatrix()
-
     num = int(input("\n"))
-
     diplay = ShowMatrix(matrix)
     multiply = ScalarMultiply(matrix, num)
