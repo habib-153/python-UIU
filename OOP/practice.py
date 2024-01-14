9705d07dfac8ef943e1541e53f6355c4a42084c0
habib-153 <h.r.sihab155@gmail.com>
2024-01-14 14:42:30 +0600

    new class added

--- OOP/practice.py
+++ OOP/practice.py
@@ -17,11 +17,31 @@ class Car:
         self.name = name      #instance variable
         self.model = model
         self.wheel = 4
-    def view(self):           # instance method
-        print(self.name, self.wheel, self.model)
+    # def view(self):           # instance method
+    #     print(self.name, self.wheel, self.model)
 
 C1 = Car("BMW", 2016)
 C2 = Car("Audi", 2017)
 
-C1.view()
-C2.view()
+# C1.view()
+# C2.view()
+
+# ===========================================================================
+#                              14.1.24
+
+class Dog:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+    
+    def update_color(self, color):
+        self.color = color
+    def poke(self):
+        print(self.color, self.name)
+
+d1 = Dog("max", "white")
+d2 = Dog("Tom", "Brown")
+
+d1.update_color("Black&white")
+
+# d1.poke()
