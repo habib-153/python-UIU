7a3aba2056db7a55e593bab70eaf754c1681f957
habib-153 <h.r.sihab155@gmail.com>
2024-01-18 11:29:02 +0600

    [methood overloading]

--- OOP/class.py
+++ OOP/class.py
@@ -2,3 +2,7 @@ class my_calculator:
     def product(self,num1, num2):
         print(num1*num2)
 
+#  same name er method but different number of parameter -- > method overloading
+c1 = my_calculator()
+c1.product(4,5)
+c1.product(4,3,5)
