4eeeab21216f9adbf677179eac64f948cae4936e
habib-153 <h.r.sihab155@gmail.com>
2024-01-21 00:01:40 +0600

    vaccine clasd

---
+++ OOP/vaccineclass.py
@@ -0,0 +1,33 @@
+class Vaccine:
+    def __init__(self,name,md_in, interval):
+        self.name = name
+        self.md_in = md_in
+        self.interval = interval
+
+class Person:
+    def __init__(self, name, age, ptype):
+        self.name = name
+        self.age = age
+        self.ptype = ptype
+        self.vac = ""
+        self.firstDose = False
+        self.secDose = False
+    def pushVaccine(self, vaCn, dose='1st dose'):
+        if dose == '1st dose':
+            if self.age >= 25 or self.ptype == 'Student':
+                self.vac=vaCn
+                self.firstDose = True
+                print("1st dose Done for", self.name)
+            else:
+                print("Sorry", self.name,". Minimum age for taking vaccine is 25")
+        else:
+            if self.vac.name != vaCn:
+                print("Sorry", self.name, "You can't take two different vaccine")
+            elif self.firstDose == True:
+                self.secDose = True
+                print("2nd dose done")
+    
+# ===========
+astra = Vaccine("Astrazeneca", "UK", 60)
+modr = Vaccine("Moderna", "UK", 30)
+sin = Vaccine("Sinopharm", "China", 30)
