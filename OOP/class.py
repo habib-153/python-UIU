4ca1aeb603e4ee668a6a72b3a5d541fb94156804
habib-153 <h.r.sihab155@gmail.com>
2024-01-20 11:01:00 +0600

    constructor overloading

--- OOP/class.py
+++ OOP/class.py
@@ -10,5 +10,19 @@ class my_calculator:
 # @dispatch(int, int, int) --> Another option for fixing method overloading
 #  same name er method but different number of parameter -- > method overloading
 c1 = my_calculator()
-c1.product(4,5)
-c1.product(4,3,5)
+# c1.product(4,5)
+# c1.product(4,3,5)
+
+# ============================ constructor Overloading==========
+
+class Student:
+    # def __init__(self, name, id):
+    #     self.name = name
+    #     self.id = id
+    def __init__(self, name, id, cg):
+        self.name = name
+        self.id = id
+        self.cg = cg
+
+# c1 = Student("carel", 22)
+c2 = Student("Ab", 33, 4)
