f9d440d85ffd6f46d5c582fdad16ec9a8209d681
habib-153 <h.r.sihab155@gmail.com>
2024-03-01 12:30:12 +0600

    OOP vaccine class

--- OOP/vaccineclass.py
+++ OOP/vaccineclass.py
@@ -14,14 +14,14 @@ class Person:
         self.secDose = False
     def pushVaccine(self, vaCn, dose='1st dose'):
         if dose == '1st dose':
-            if self.age >= 25 or self.ptype == 'Student':
+            if self.age >= 25 or self.ptype == 'Student' or self.ptype == 'student' :
                 self.vac=vaCn
                 self.firstDose = True
                 print("1st dose Done for", self.name)
             else:
                 print("Sorry", self.name,". Minimum age for taking vaccine is 25")
         else:
-            if self.vac.name != vaCn:
+            if self.vac.name != vaCn.name:
                 print("Sorry", self.name, "You can't take two different vaccine")
             elif self.firstDose == True:
                 self.secDose = True
@@ -31,3 +31,6 @@ class Person:
 astra = Vaccine("Astrazeneca", "UK", 60)
 modr = Vaccine("Moderna", "UK", 30)
 sin = Vaccine("Sinopharm", "China", 30)
+Mahi = Person("Mahi", 18, "student")
+Mahi.pushVaccine(sin)
+print(Mahi.vac.name)
