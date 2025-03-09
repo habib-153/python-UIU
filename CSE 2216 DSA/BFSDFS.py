254e630e96534ba0004a3864165ff2000ca51ec1
habib-153 <h.r.sihab155@gmail.com>
2025-03-10 00:26:12 +0600

    Remove unused audio and image assets from the project

--- CSE 2216 DSA/BFSDFS.py
+++ CSE 2216 DSA/BFSDFS.py
@@ -30,3 +30,29 @@ graph = {'0': set(['1', '2']),
 
 print(dfs(graph, '0'))
 print(bfs(graph, 0))
+
+
+def find_max_weight_cell(exits):
+    n = len(exits)
+    weights = [0] * n
+
+    for i in range(n):
+        if exits[i] != -1:
+            weights[exits[i]] += i
+
+    max_weight = -1
+    max_weight_cell = -1
+
+    for i in range(n):
+        if weights[i] > max_weight or (weights[i] == max_weight and i > max_weight_cell):
+            max_weight = weights[i]
+            max_weight_cell = i
+
+    return max_weight_cell
+
+
+# Example usage:
+exits1 = [2, 0, -1, 2]
+exits2 = [-1]
+print(find_max_weight_cell(exits1))  # Output: 2
+print(find_max_weight_cell(exits2))  # Output: 0
