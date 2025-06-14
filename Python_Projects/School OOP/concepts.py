00581f65363276af062cc33918ff37c92f80ce45
Habibur Rahman <h.r.sihab155@gmail.com>
2025-06-14 23:18:41 +0600

    Remove Flask application and School OOP project files, including main logic, person classes, and related functionalities. Delete test script and weather data CSV file.

--- Python_Projects/School OOP/concepts.py
+++
@@ -1,9 +0,0 @@
-"""
-Core Functionalities
-1. There will be a classroom consisting of multiple students
-2. Student can enroll multiple classes
-3. Teachers will take classes, they will take exams, they grade the exams
-4. System will generate the overall grade
-
-"""
-
